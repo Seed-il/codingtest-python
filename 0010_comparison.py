64e3c91f4782dc14d58867a8c43302698a2d2d3d
Seed-il <seedone1998@gmail.com>
2024-01-18 23:29:21 +0900

    12th

--- 0010_comparison.py
+++ 0010_comparison.py
@@ -1,13 +1,23 @@
+"""
+Source : Seed_il(Seed_one)
+
+Theme : [Coding Test - Python]
+
+Q-0010
+-> Input two numbers A, B
+and Compare them
+"""
+
 start = True
 
 while(start):
-    
+
     try:
         a, b = map(int, input().split())
 
-        if a > b :
+        if a > b:
             print(">")
-        elif a < b :
+        elif a < b:
             print("<")
         elif a == b:
             print("==")
--- 0011_score.py
+++ 0011_score.py
@@ -1,20 +1,29 @@
+"""
+Source : Seed_il(Seed_one)
+Theme : [Coding Test - Python]
+
+Q-0011
+-> Input a score, and classify it
+(A ~ F)
+"""
+
 start = True
 
-while(start) :
+while(start):
 
-    try :
+    try:
         score = int(input())
 
-        if score >= 90 :
+        if score >= 90:
             print("A")
-        elif score >= 80 :
+        elif score >= 80:
             print("B")
-        elif score >= 70 :
+        elif score >= 70:
             print("C")
-        elif score >= 60 :
+        elif score >= 60:
             print("D")
         else:
             print("F")
 
-    except : 
+    except:
         start = False
