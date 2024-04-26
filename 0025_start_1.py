9b4500b649648ca29e85610662eefd548922fbb4
Seed-il <seedone1998@gmail.com>
2024-04-26 23:21:54 +0900

    0025 stars ver1 commit

---
+++ 0025_start_1.py
@@ -0,0 +1,27 @@
+'''
+Source : Seed_il(Seed_one)
+Theme : [Coding Test - Python]
+
+Q-0025
+-> Print Stars
+'''
+import sys
+
+n = int(sys.stdin.readline())
+
+for i in range(1, n+1) : 
+    for j in range(i) : 
+        print("*", end="")
+    print()
+
+
+'''
+5
+
+=>
+*
+**
+***
+****
+*****
+'''
