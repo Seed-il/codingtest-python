6b4ccd8dc0894e916661d0143232000d83e165d6
Seed-il <seedone1998@gmail.com>
2024-05-31 00:30:39 +0900

    0040 string length commit

---
+++ 0040_string_length.py
@@ -0,0 +1,15 @@
+'''
+Source : Seed_il(Seed_one)
+Theme : [Coding Test - Python]
+
+Q-0040
+-> String Length.
+Input a word, 
+and print out the length of this word.
+'''
+
+import sys
+
+s = str(sys.stdin.readline())
+
+print(len(s)-1)
