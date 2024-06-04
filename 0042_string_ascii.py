14965cc916767b3d08f439472b051281fc25e701
Seed-il <seedone1998@gmail.com>
2024-06-04 22:44:25 +0900

    0039 string order commit

---
+++ 0042_string_ascii.py
@@ -0,0 +1,26 @@
+'''
+Source : Seed_il(Seed_one)
+Theme : [Coding Test - Python]
+
+Q-0042
+-> String ASCII.
+Input a word, 
+and print out 
+the ASCII code about the word.
+
+ex_1)           | ex_2)
+A               | C
+                |
+=> 65           | => 67
+ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+ex_3)           | ex_4)
+0               | a
+                |
+=> 48           | => 97
+'''
+
+import sys
+
+n = sys.stdin.readline().rstrip()
+
+print(ord(n))
