e7e6a1cd2c026ce7845436c22cc88a24300becea
Seed-il <seedone1998@gmail.com>
2024-06-25 22:54:29 +0900

    0049 string follow commit

---
+++ 0049_string_follow.py
@@ -0,0 +1,25 @@
+'''
+Source : Seed_il(Seed_one)
+Theme : [Coding Test - Python]
+
+Q-0049
+-> String Follow.
+Enter the string and print it out as it is.
+
+
+ex)           
+Hello
+Python
+Online Lesson
+                
+=>
+Hello
+Python
+Online Lesson
+'''
+
+while True : 
+    try : 
+        print(input())
+    except : 
+        break
