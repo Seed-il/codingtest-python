e54d681f6986d8c7818ea1616bd113f6aa8df1d0
Seed-il <seedone1998@gmail.com>
2024-06-25 23:18:44 +0900

    0050 string leaves commit

---
+++ 0050_leaves.py
@@ -0,0 +1,26 @@
+'''
+Source : Seed_il(Seed_one)
+Theme : [Coding Test - Python]
+
+Q-0050
+-> String Leaves.
+Print out leaves.
+
+'''
+'''
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+'''
+
+leaves = """         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |"""
+
+print(leaves)
