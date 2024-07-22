92bb19e8c5c93d89de93c45b153a8694dfa35395
Seed-il <seedone1998@gmail.com>
2024-07-22 22:36:26 +0900

    0055 croatian alphabet commit

---
+++ 0055_croatian_alphabet.py
@@ -0,0 +1,29 @@
+'''
+Source : Seed_il(Seed_one)
+Theme : [Coding Test - Python]
+
+Q-0055
+-> Croatian alphabet.
+Enter the alphabet and special characters, 
+and print out how many Croatian alphabets 
+were used in total.
+
+ex_1)           | ex_2)
+ljes=njak       | ddz=z=
+                |
+=> 6            | => 3
+ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+ex_3)           | ex_4)
+nljj            | c=c=
+                |
+=> 3            | => 2
+'''
+
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
+word = input()
+
+for i in croatia : 
+    word = word.replace(i, '*')
+    
+print(len(word))
